be57444981bb6cc7f6c53aafdd12398d070f6bf2
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2017-03-14 13:09:24 -0300

    Added operation comparison to exc8

--- src/TP1/Exercise8.py
+++ src/TP1/Exercise8.py
@@ -1,7 +1,18 @@
+"""
+    Takes an array of the following form [a0, a1, ... an] representing a polynomial
+    and a number for 'x'. Returns the value of the polynomial when evaluated with that 'x'.
+"""
+
+
 def horner(array, x):
+    # counts the number of multiplications and sums for comparison
+    amount_of_operations = 0
     s = array[0]
     for i in range(1, len(array)):
         s = s * x + array[i]
+        # one sum and multiplication each time
+        amount_of_operations += 2
+    print("Amount of operations: ", amount_of_operations)
     return s
 
 
@@ -13,3 +24,15 @@ def horner_r_aux(array, x, i, s):
     if i == len(array):
         return s
     return horner_r_aux(array, x, i + 1, s * x + array[i])
+
+
+def evaluate_polynomial(array, x):
+    # counts the number of multiplications and sums for comparison
+    amount_of_operations = 0
+    result = 0
+    for i in range(0, len(array)):
+        result += array[0]*(x**i)
+        # x**i counts as i multiplications, then we multiply by the n term and we sum it all
+        amount_of_operations += i + 2
+    print("Amount of operations: ", amount_of_operations)
+    return result
--- src/TP1/Tests/Exc8Test.py
+++ src/TP1/Tests/Exc8Test.py
@@ -1,3 +1,4 @@
 from src.TP1.Exercise8 import *
 print("Horner [10,5,8,1] x = 3: {}" .format(horner([10, 5, 8, 1], 3)))
 print("Recursive Horner [10,5,8,1] x = 3: {}" .format(horner_r([10, 5, 8, 1], 3)))
+print("Regular evaluation [10,5,8,1] x = 3: {}" .format(evaluate_polynomial([10, 5, 8, 1], 3)))
