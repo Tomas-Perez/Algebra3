5044f5ef124d1da138f2834bb77cf206cec0d617
Ariel Mirra <ariel.mirra@ing.austral.edu.ar>
2017-03-21 12:46:37 -0300

    added practice 2 class and exercise 8

---
+++ src/TP2/Exercise8.py
@@ -0,0 +1,9 @@
+"""
+calculates the sum of j, from m to n
+"""
+
+
+def sum8(m, n, j):
+    result = 0
+    for j in range(m, n+1):
+        result += j
---
+++ src/TP2/Practice2.py
@@ -0,0 +1,20 @@
+class Practice2:
+    def exercise8(m, n):
+        # Implement exercise 8
+        return 
+
+    def exercise9(m, n, r, s):
+        # Implement exercise 9
+        return 
+
+	def exercise10(n):
+        # Implement exercise 10
+        return 
+
+	def exercise11Sumation(n, x);
+        # Implement exercise 11 sumation
+        return
+
+    def exercise11Formula(n, x);
+        # Implement exercise 11 formula
+        return 
