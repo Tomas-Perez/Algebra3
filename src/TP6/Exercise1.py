ed5aa1a46255f8a960e88f29eb26d25bea6acf6f
Ariel Mirra <ariel.mirra@ing.austral.edu.ar>
2017-05-09 11:47:08 -0300

    ex 1 e & test

--- src/TP6/Exercise1.py
+++ src/TP6/Exercise1.py
@@ -30,6 +30,7 @@ def secondary_diag_sum(matrix):
 
 
 """
+1c)
 creates an array in which each element is the sum of every row of a rectangular matrix
 """
 
@@ -41,4 +42,20 @@ def ex1c(matrix):
         for j in range(len(matrix)):
             result[i] += matrix[i][j]
     return result
+
+
+"""
+1e)
+given two matrix n*m, returns their sum
+"""
+
+
+def ex1e(matrix1, matrix2):
+    result = []
+    for n in range(len(matrix1[0])):
+        result.append([])
+        for m in range(len(matrix1)):
+            result[n].append(matrix1[n][m] + matrix2[n][m])
+    return result
+
 
--- src/TP6/Tests/Exercise1Test.py
+++ src/TP6/Tests/Exercise1Test.py
@@ -1,8 +1,18 @@
 from src.TP6.Exercise1 import *
 
 matrix1 = [[1, 2], [2, 1]]
+matrix2 = [[-1, -2], [-2, -1]]
 
-assert 2 == diag_sum(matrix1)
-assert 4 == secondary_diag_sum(matrix1)
+#assert 2 == diag_sum(matrix1)
+#assert 4 == secondary_diag_sum(matrix1)
+
+"""
+1c)
+"""
 assert ex1c(matrix1)[0] == 3
 
+
+"""
+1e)
+"""
+assert ex1e(matrix1, matrix2)[0][0] == 0
