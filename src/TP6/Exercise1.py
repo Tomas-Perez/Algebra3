06cac3fc36d17adb88cdfe4d21a0276184f2e669
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2017-05-09 11:08:54 -0300

    Pushed testing file, ariel is black

--- src/TP6/Exercise1.py
+++ src/TP6/Exercise1.py
@@ -1,4 +1,9 @@
 """
+Contains algorithms for solving Exercise 1
+"""
+
+
+"""
 Calculates the sum of the diagonal of a square matrix
 """
 
@@ -6,11 +11,8 @@ Calculates the sum of the diagonal of a square matrix
 def diag_sum(matrix):
     matrix_size = len(matrix)
     result = 0
-    if matrix_size != len(matrix[0]):
-        for i in range(matrix_size):
-            result += matrix[i][i]
-    else:
-        print("The matrix is not square")
+    for i in range(matrix_size):
+        result += matrix[i][i]
     return result
 
 
@@ -22,10 +24,7 @@ Calculates the sum of the diagonal of a square matrix
 def secondary_diag_sum(matrix):
     matrix_size = len(matrix)
     result = 0
-    if matrix_size != len(matrix[0]):
-        for i in range(matrix_size):
-            result += matrix[matrix_size-i][i]
-    else:
-        print("The matrix is not square")
+    for i in range(matrix_size):
+        result += matrix[matrix_size-i][i]
     return result
 
---
+++ src/TP6/Tests/Exercise1Test.py
@@ -0,0 +1,6 @@
+from src.TP6.Exercise1 import *
+
+matrix1 = [[1,2],[2,1]]
+
+assert 2 == diag_sum(matrix1)
+assert 4 == secondary_diag_sum(matrix1)
