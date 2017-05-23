d416936597f07f1c874a34f8eb38b519cdaaf4df
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2017-05-23 10:59:40 -0300

    Added base classes

---
+++ src/TP7/TP4.py
@@ -0,0 +1,41 @@
+class TP4:
+
+    # Returns double[]. coefficients is of type double[][]. independentTerms is of type double[].
+    def exercise1(self, coefficients, independent_terms):
+        # Implement algorithm
+        return 
+
+    # Returns double[]. coefficients is of type double[][]. independentTerms is of type double[].
+    def exercise2(self, coefficients, independent_terms):
+        # Implement algorithm
+        return 
+
+    # Returns double[]
+    def exercise5WithoutPivoteo(self, coefficients, independent_terms):
+        # Implement algorithm
+        return 
+
+    # Returns double[]. coefficients is of type double[][]. independentTerms is of type double[].
+    def exercise5PartialPivoteo(self, coefficients, independent_terms):
+        # Implement algorithm
+        return 
+
+    # Returns double[]. coefficients is of type double[][]. independentTerms is of type double[].
+    def exercise6(self, coefficients, independent_terms, calculator):
+        # Implement algorithm
+        return 
+
+    # Returns double[]. coefficients is of type double[][]. independentTerms is of type double[].
+    def exercise7(self, coefficients, independent_terms, calculator):
+        # Implement algorithm
+        return 
+
+    # Returns double[][]. coefficients is of type double[][].
+    def exercise8(self, coefficients):
+        # Implement algorithm
+        return 
+
+    # Returns double[]. coefficients is of type double[][]. independentTerms is of type double[]. 
+    def exercise9(self, coefficients, independent_terms):
+        # Implement algorithm
+        return
